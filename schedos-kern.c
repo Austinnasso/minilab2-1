8163913545d602c572864ccc61f146e6a577135f
Austin Nasso <austinasso@gmail.com>
2015-05-22 17:28:27 -0400

    Test

--- schedos-kern.c
+++ schedos-kern.c
@@ -160,13 +160,13 @@ interrupt(registers_t *reg)
 		if (reg->reg_eax == -1)
 		  {
 		    if (current->p_pid == 1)
-		      current->p_priority = 7;
+		      current->p_priority = 3;
 
 		    if (current->p_pid == 2)
-		      current->p_priority = 14;
+		      current->p_priority = 4;
 
 		    if (current->p_pid == 3)
-		      current->p_priority = 14;
+		      current->p_priority = 5;
 
 		    if (current->p_pid == 4)
 		      current->p_priority = 7;
